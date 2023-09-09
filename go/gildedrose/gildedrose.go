d9d433cfc9147d518d082f71b3b1a1ac97178125
Christian Lascsak <christian.lascsak7@gmail.com>
2023-09-09 17:54:21 +0200

    fat refactor of main processing

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -11,49 +11,59 @@ type Item struct {
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
-		if items[i].Name != AGED_BRIE && items[i].Name != BACKSTAGE {
-			if items[i].Quality > 0 {
-				if items[i].Name != SULFURAS {
-					items[i].Quality = items[i].Quality - 1
-				}
-			}
-		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == BACKSTAGE {
-					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-				}
+		process(items[i])
+	}
+}
+
+func process(item *Item) {
+	if item.Name != AGED_BRIE && item.Name != BACKSTAGE {
+		if item.Quality > 0 {
+			if item.Name != SULFURAS {
+				item.Quality = item.Quality - 1
 			}
 		}
+	} else {
+		increaseQuality(item)
+	}
 
-		if items[i].Name != SULFURAS {
-			items[i].SellIn = items[i].SellIn - 1
-		}
+	if item.Name != SULFURAS {
+		item.SellIn = item.SellIn - 1
+	}
 
-		if items[i].SellIn < 0 {
-			if items[i].Name != AGED_BRIE {
-				if items[i].Name != BACKSTAGE {
-					if items[i].Quality > 0 {
-						if items[i].Name != SULFURAS {
-							items[i].Quality -= 1
-						}
+	if item.SellIn < 0 {
+		if item.Name != AGED_BRIE {
+			if item.Name != BACKSTAGE {
+				if item.Quality > 0 {
+					if item.Name != SULFURAS {
+						item.Quality -= 1
 					}
-				} else {
-					items[i].Quality -= items[i].Quality
 				}
 			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality += 1
-				}
+				item.Quality -= item.Quality
+			}
+		} else {
+			if item.Quality < 50 {
+				item.Quality += 1
+			}
+		}
+	}
+}
+
+func increaseQuality(item *Item) {
+	if item.Quality >= 50 {
+		return
+	}
+
+	item.Quality = item.Quality + 1
+	if item.Name == BACKSTAGE {
+		if item.SellIn < 11 {
+			if item.Quality < 50 {
+				item.Quality = item.Quality + 1
+			}
+		}
+		if item.SellIn < 6 {
+			if item.Quality < 50 {
+				item.Quality = item.Quality + 1
 			}
 		}
 	}
