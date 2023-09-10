f6229e665c51f73a198b61bdc13e39d08f8086e5
Christian Lascsak <christian.lascsak7@gmail.com>
2023-09-10 10:13:44 +0200

    run 50 iterations for testing to get close to quality 50 threshold

--- go/tests/test_fixture.sh
+++ go/tests/test_fixture.sh
@@ -1,3 +1,3 @@
-go run texttest_fixture.go 10 > tests/test_10_days.txt
+go run texttest_fixture.go 50 > tests/test_50_days.txt
 
-diff tests/original_output_10_days.txt tests/test_10_days.txt 
+diff tests/original_output_50_days.txt tests/test_50_days.txt 
